434e35286c3a994df3956b81d9d326e6424bcc53
Kevin Axxel Romero Carrillo <88946816+KevinRomero2@users.noreply.github.com>
2023-03-04 17:32:28 -0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    //Arreglos
+   /* val diasSemana = arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
+    println(diasSemana[2])
+    diasSemana.set(2, "Miernes")
+    println(diasSemana[2])
+    if (diasSemana.size == 7) {
+        println(diasSemana[6])
+    }
+
+    diasSemana.forEach {
+        println(it)
+        if (it == "Jueves") {
+            println("Por fin es jueves")
+        }
+    }
+
+    println(diasSemana.first())
+    println(diasSemana.last())     */
+
+    //listas
+    
+}
